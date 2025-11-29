7e783407329eeef444dea862a9f2b6e125fb91ef
justin <justin.threlkeld@gmail.com>
2025-11-29 14:16:20 -0700

    checking in configs as they exist today

---
+++ dots/private_dot_config/private_fish/config.fish
@@ -0,0 +1,11 @@
+# source ~/.profile
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set --export STARSHIP_CONFIG ~/.config/starship.toml
+starship init fish | source
+
+if [ -f ~/.aliases ]
+    source ~/.aliases
+end
