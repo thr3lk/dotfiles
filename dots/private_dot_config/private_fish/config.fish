76d8c0d4b10306070fe51c1ac433e040b188619e
justin <justin@threlk.net>
2025-11-29 19:05:53 -0700

    standarize the shell configs a bit

--- dots/private_dot_config/private_fish/config.fish
+++ dots/private_dot_config/private_fish/config.fish
@@ -1,7 +1,11 @@
+mise activate fish | source
+
 # source ~/.profile
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+
 
 set --export STARSHIP_CONFIG ~/.config/starship.toml
 starship init fish | source
